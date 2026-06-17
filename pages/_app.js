2434055248b9a6d132d10b9469c2af66aac8a120
cp3605648-maker <cp3605648@gmail.com>
2026-06-17 23:21:10 +0000

    Add Meta Pixel

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import GoogleAnalytics from "../components/GoogleAnalytics";
+import MetaPixel from "../components/MetaPixel";
 import "../styles/responsive.css";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <GoogleAnalytics />
+      <MetaPixel />
       <Component {...pageProps} />
     </>
   );
