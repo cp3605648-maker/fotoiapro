d688c9bafff8dfff60ebe54cb201b7536aaeff25
cp3605648-maker <cp3605648@gmail.com>
2026-06-19 01:37:41 +0000

    Create dynamic sitemap route

---
+++ pages/sitemap.xml.js
@@ -0,0 +1,37 @@
+const SITE_URL = "https://www.fotoia.pro";
+
+function generateSitemap() {
+  const routes = [
+    "",
+    "/login",
+    "/contact",
+    "/privacy",
+    "/terms",
+    "/refunds",
+  ];
+
+  return `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${routes
+  .map((route) => {
+    return `  <url>
+    <loc>${SITE_URL}${route}</loc>
+  </url>`;
+  })
+  .join("\n")}
+</urlset>`;
+}
+
+export async function getServerSideProps({ res }) {
+  res.setHeader("Content-Type", "text/xml");
+  res.write(generateSitemap());
+  res.end();
+
+  return {
+    props: {},
+  };
+}
+
+export default function Sitemap() {
+  return null;
+}
